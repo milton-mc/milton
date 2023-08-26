4c993017daa0dec54b24bcb509acc8aae94715e1
Bad name <67664504+no-good-names@users.noreply.github.com>
2023-08-25 23:16:09 -0500

    Created miltonModMixin.java and Utils.java

--- src/main/java/com/milton/mixin/miltonModMixin.java
+++ src/main/java/com/milton/mixin/miltonModMixin.java
@@ -1,8 +1,5 @@
 package com.milton.mixin;
 
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
